e075f4bc46fdd8da18db2af6a549917d3648a7a1
James Eddy <james.a.eddy@gmail.com>
2018-03-23 10:31:05 -0700

    move any plot/chart formatting code to format.R

---
+++ iatlas-demo/functions/format.R
@@ -0,0 +1,10 @@
+# common plot theme
+theme_1012 <- theme(
+  axis.text = element_text(face = "bold", size = 10, color = "black"),
+  axis.title = element_text(face = "bold", size = 12, color = "black"),
+  panel.border = element_rect(colour = "black", size = 1),
+  strip.text = element_text(face = "bold", size = 10, color = "black"),
+  strip.background = element_rect(colour = "black", size = 1),
+  title = element_text(face = "bold", size = 14, color = "black"),
+  legend.text = element_text(face = "bold", size = 8, color = "black")
+)
