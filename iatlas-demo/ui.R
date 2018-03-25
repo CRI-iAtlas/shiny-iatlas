b9fc37fbd6b05745ec38da6ce136d82008947176
James Eddy <james.a.eddy@gmail.com>
2018-03-24 21:09:52 -0700

    add navbarpage wrapper around dashboard

--- iatlas-demo/ui.R
+++ iatlas-demo/ui.R
@@ -1,9 +1,13 @@
-dashboardPage(
-    dashboardHeader(title = "iATLAS"),
+################################################################################
+# Define each fluid page
+################################################################################
+
+explorepage <- dashboardPage(skin = "black",
+    dashboardHeader(disable = TRUE),
     dashboardSidebar(
         sidebarMenu(
             menuItem(
-                "Dashboard",   
+                "iAtlas Explorer",   
                 tabName = "dashboard", 
                 icon = icon("dashboard")),
             menuItem(
@@ -31,7 +35,7 @@ dashboardPage(
     dashboardBody(
         tabItems(
             tabItem(tabName = "dashboard",
-                    h2("Welcome to the iATLAS portal prototype!")
+                    h2("Welcome to the iAtlas Portal prototype!")
             ),
             tabItem(tabName = "cell_content",
                     cellcontent_UI("module1")
@@ -51,3 +55,29 @@ dashboardPage(
         )
     )
 )
+
+################################################################################	
+# Define general header tag list	
+# List of tags to display as a common header above all tabPanels.	
+################################################################################
+
+headerTagList <- list(	
+  tags$style(type = "text/css", ".navbar .navbar-nav {float: right; font-size: 14px} .navbar .navbar-nav li a {font-size: 14px} .nav-tabs {font-size: 12px}"),	
+  tags$base(target = "_blank")	
+)
+
+################################################################################	
+# Define the full user-interface, `ui`	
+################################################################################
+
+ui <- navbarPage(	
+  title = strong("CRI iAtlas Portal"), selected = "Explore",	
+  tabPanel("Explore", explorepage, icon = icon("bar-chart")),	
+  tabPanel("Documentation", icon = icon("file-text")),	
+  tabPanel("Help", icon = icon("question")),	
+  header = headerTagList,	
+  collapsible = TRUE,	
+  windowTitle = "iATLAS"	
+)	
+
+shinyUI(ui)
