c6ffd2c25e25ff4dcaa4c7749dec872bff05b0db
andrewelamb <andrewelamb@gmail.com>
2018-10-29 14:40:14 -0700

    remove print statements

--- functions/kmplot.R
+++ functions/kmplot.R
@@ -1,7 +1,5 @@
 create_kmplot <- function(fit, df, confint, risktable, title, group_colors) {
-    print(group_colors)
     group_colors <- set_names(group_colors, NULL)
-    print(group_colors)
     survminer::ggsurvplot(
         fit,
         data = df,
