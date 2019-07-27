2fdf0a56c2e5c2d550162c6b4fe09c321a767ed8
andrewelamb <andrewelamb@gmail.com>
2019-07-27 08:15:46 -0700

    moved html to markdown

--- modules/drivermodule.R
+++ modules/drivermodule.R
@@ -5,27 +5,17 @@ drivers_UI <- function(id) {
         titleBox("iAtlas Explorer — Association with Driver Mutations"),
         textBox(
             width = 12,
-            p("This module can be used to test whether an immune readout is statistically associated with ",ncol(panimmune_data$driver_mutation_df)-1," cancer driver mutations, within each of your sample groups.")  
+            p(
+                "This module can be used to test whether an immune readout is statistically associated with ",
+                ncol(panimmune_data$driver_mutation_df) - 1,
+                " cancer driver mutations, within each of your sample groups."
+            )  
         ),
         sectionBox(
             title = "Immune Response Association With Driver Mutations",
             messageBox(
                 width = 12,
-                p("This plot displays the degree of association between driver mutation in samples and an immune readout, as determined by the Select Response Variable option. Every point in the scatter plot corresponds to a comparison of the values of that immune readout in samples in which a particular driver gene is mutated to the values in samples in which is not. This comparison is made within each cohort among the Sample Groups. Each point thus corresponds to a single driver gene and cohort. The driver-cohort combination can be seen by hovering on a point (separated by a dot)."),
-                tags$ul(
-                  tags$li("The x-axis show the effect size, defined as the ratio of the mean readout value in mutated vs non-mutated samples."),
-                  tags$li("The y-axis represents the P-value of the significance test comparing the readout in mutated vs non-mutated samples. A line is drawn for P=0.05, with the more significant values above that line")
-                ),
-                p("Manuscript context: This allows you to display distributions such as those shown in Figure 4D.","\n"),
-                p("Click on a point to see a violin plot for the immune readout value distribution in mutated vs non-mutated samples for the selected cohort and driver."),
-                p(""),
-                p("A statistical test is performed in a group only when the number of mutant samples exceeds a minimum required count (currently 3). In rare instances all (or all but one) samples within a group contain the mutation and a test cannot be performed."),
-                p(""),
-                p("Please Note: This is an initial version of this module. It works best with moderate group sizes. Multiple hypothesis testing correction and incorporation of covariates will be added at a later stage."),
-                p(""),
-                p("Driver mutations from Bailey, Tokheim, Porta-Pardo et al. (2018),",
-                  em("Comprehensive Characterization of Cancer Driver Genes and Mutations"),
-                  "Cell 173. https://doi.org/10.1016/j.cell.2018.02.060.")
+                includeMarkdown("data/markdown/driver_single.markdown")
             ),
             fluidRow(
                 optionsBox(
