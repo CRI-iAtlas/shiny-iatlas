cd0e64a910a916e4ab475c9f604c658d3e83b28d
Shane Brinkman-Davis Delamore <shanebdavis@gmail.com>
2019-10-25 13:06:14 -0700

    WIP - making it easy to build from scratch

--- setup_installs.R
+++ setup_installs.R
@@ -1,26 +1,19 @@
-
 library(githubinstall)
 library(BiocManager)
 
 deps <- read.table('DEPENDENCIES', header=T, stringsAsFactors = F)
 
-for (i in 25:nrow(deps)) 
-{
+for (i in 1:nrow(deps)) {
   installed <- require(deps$package[i], character.only = T)
   if (!installed) {
-    
-    if (deps$source[i] == 'CRAN') 
-    {
-      install.packages(deps$package[i])} 
-    else if (deps$source[i] == 'github')
-    {
-      githubinstall::githubinstall(packages = deps$package[i], ask = F)
-    } 
-    else 
-    {
+    if (deps$source[i] == 'CRAN') {
+      install.packages(deps$package[i])
+
+    } else if (deps$source[i] == 'github') {
+      devtools::install_github(deps$package[i], ask = F)
+
+    } else {
       BiocManager::install(deps$package[i])
-    } 
-    
+    }
   }
 }
-
