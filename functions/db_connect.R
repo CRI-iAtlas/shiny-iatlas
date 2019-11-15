b1bbfc203f939795f75bd69f7fb3d6567ef14e49
Jon Ryser <241263+jonryser@users.noreply.github.com>
2019-11-15 15:13:27 -0800

    Spaces man!

--- functions/db_connect.R
+++ functions/db_connect.R
@@ -1,7 +1,7 @@
 # Create the database connection.
 create_db_connection <- function() {
     con = con <- dbConnect(
-        RPostgres::Postgres(), 
+        RPostgres::Postgres(),
         dbname = DB_NAME, # 'shiny_iatlas_dev',
         host = DB_HOST, # 'localhost',
         port = DB_PORT, # 5432,
