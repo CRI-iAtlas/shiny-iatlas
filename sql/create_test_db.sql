58393ab8082fd77050b737bf32b0d5f6dc33b238
Jon Ryser <241263+jonryser@users.noreply.github.com>
2019-11-26 09:08:44 -0800

    SQL to create DB and tables for dev and test databases.

--- sql/create_test_db.sql
+++ sql/create_test_db.sql
@@ -1,13 +1,13 @@
 -- Terminate activity on DB before dropping it. See https://stackoverflow.com/questions/7073773/drop-postgresql-database-through-command-line
-SELECT pg_terminate_backend(pid) FROM pg_stat_activity WHERE datname='iatlas_shiny_dev';
--- Drop the dev database if it already exists.
-DROP DATABASE IF EXISTS iatlas_shiny_dev;
+SELECT pg_terminate_backend(pid) FROM pg_stat_activity WHERE datname='iatlas_shiny_test';
+-- Drop the test database if it already exists.
+DROP DATABASE IF EXISTS iatlas_shiny_test;
 
--- Create the dev database (used for development).
-CREATE DATABASE iatlas_shiny_dev;
+-- Create the test database (used for running tests).
+CREATE DATABASE iatlas_shiny_test;
 
 -- Connect to the database.
-\connect iatlas_shiny_dev
+\connect iatlas_shiny_test
 
 -- Create STATUS_ENUM ENUM
 DO $$ BEGIN
