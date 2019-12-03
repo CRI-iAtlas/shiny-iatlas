bc99192eb6d13800f1042ac4b9872dc09adc5496
Jon Ryser <241263+jonryser@users.noreply.github.com>
2019-12-03 13:23:22 -0800

    Ability to pass an argument that informs the script whether to completely reset the DB or not.

--- scripts/create_db.sh
+++ scripts/create_db.sh
@@ -1,12 +1,43 @@
 #!/bin/sh
 
-# env can be dev or test. If no argument is passed, it will default to dev.
-env=${1:-dev}
+# env can be dev or test. If no env argument is passed as the first argument (not dev or test), it will default to dev.
+case $1 in
+    dev | test)
+        env=$1
+    ;;
+    
+    *)
+        env=dev
+    ;;
+esac
+
+# If the first argument passed is create or reset, the DB and tables will be build, wiping out any existing DB and tables.
+case $1 in
+    create | reset)
+        reset=true
+    ;;
+    
+    *)
+        # If an env argument is passed as the first argument and the second argument is create or reset, the DB and tables will be build, wiping out any existing DB and tables.
+        case $2 in
+            create | reset)
+                reset=true
+            ;;
+            # By default, don't build the DB and tables.
+            *)
+                reset=false
+            ;;
+        esac
+    ;;
+esac
 
 YELLOW="\033[1;33m"
 GREEN="\033[0;32m"
 # No Color
 NC='\033[0m'
+
+>&2 echo -e "${GREEN}Env == ${env}${NC}"
+>&2 echo -e "${GREEN}Reset == ${reset}${NC}"
 
 # The local project directory (assumes this file is stll in a child folder of the project).
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && cd ../ && pwd )"
@@ -36,7 +67,7 @@ if [ ! "$(docker ps -q -f name=$docker_image)" ]; then
         docker rm $docker_image
     fi
     # Run the container
-    docker run --rm --name $docker_image -e POSTGRES_PASSWORD=$db_pw -d -p $db_port:$db_port -v /$db_data_dir:/var/lib/postgresql postgres:11.5
+    docker run --rm --name $docker_image -e POSTGRES_PASSWORD=$db_pw -d -p $db_port:$db_port -v /$db_data_dir:/var/lib/postgresql/data postgres:11.5
 fi
 
 >&2 echo -e "${YELLOW}Postgres is starting - please be patient${NC}"
@@ -44,10 +75,12 @@ until docker exec $docker_image psql -U $db_user  2> /dev/null; do
     sleep 1
 done
 
->&2 echo -e "${GREEN}Postgres is up - building database and tables${NC}"
-
-# Copy the SQL file into the docker container.
-docker cp $DIR/sql/$create_db_sql $docker_image:/$create_db_sql
-
-# Run the SQL script within the docker container using the docker container's psql.
-docker exec -u $db_user $docker_image psql -f //$create_db_sql
+if [ $reset == true ]; then
+    >&2 echo -e "${GREEN}Postgres is up - building database and tables${NC}"
+    
+    # Copy the database SQL file into the docker container.
+    docker cp $DIR/sql/$create_db_sql $docker_image:/$create_db_sql
+    
+    # Run the database SQL script within the docker container using the docker container's psql.
+    docker exec -u $db_user $docker_image psql -f //$create_db_sql
+fi
