e351ec739c5da4702844d9337abe63b4f376b47b
Jon Ryser <241263+jonryser@users.noreply.github.com>
2019-12-06 09:20:21 -0800

    Build features_to_samples table.

--- database/build_samples_tables.R
+++ database/build_samples_tables.R
@@ -9,6 +9,8 @@ driver_mutations4 <-
   feather::read_feather("data2/driver_mutations4.feather")
 driver_mutations5 <-
   feather::read_feather("data2/driver_mutations5.feather")
+feature_values_long <-
+  feather::read_feather("data2/feature_values_long.feather")
 immunomodulator_expr <-
   feather::read_feather("data2/immunomodulator_expr.feather")
 io_target_expr1 <-
@@ -30,13 +32,89 @@ all_samples <-
     driver_mutations3,
     driver_mutations4,
     driver_mutations5,
+    feature_values_long,
     immunomodulator_expr,
     io_target_expr1,
     io_target_expr2,
     io_target_expr3,
-    io_target_expr4,
-    io_target_expr1
-  )
+    io_target_expr4
+  ) %>%
+  dplyr::arrange(sample)
+
+# Clean up.
+rm(driver_mutations1)
+rm(driver_mutations2)
+rm(driver_mutations3)
+rm(driver_mutations4)
+rm(driver_mutations5)
+rm(feature_values_long)
+rm(immunomodulator_expr)
+rm(io_target_expr1)
+rm(io_target_expr2)
+rm(io_target_expr3)
+rm(io_target_expr4)
 
 # Get only the sample names (no duplicates).
 samples <- all_samples %>% dplyr::distinct(sample)
+
+# Build the samples table.
+samples <- samples %>%
+  dplyr::rename_at("sample", ~("sample_id")) %>%
+  merge(til_image_links, by.x = "sample_id", by.y = "sample", all = TRUE) %>%
+  dplyr::arrange(sample_id) %>%
+  dplyr::rename_at("link", ~("tissue_id")) %>%
+  dplyr::select(dplyr::everything()) %>%
+  dplyr::rowwise() %>%
+  dplyr::mutate(tissue_id = stringi::stri_extract_first(tissue_id, regex = "[\\w]{4}-[\\w]{2}-[\\w]{4}-[\\w]{3}-[\\d]{2}-[\\w]{3}"))
+samples %>% .GlobalEnv$write_table_ts(.GlobalEnv$con, "samples", .)
+samples <- RPostgres::dbReadTable(.GlobalEnv$con, "samples")
+
+# Remove the large til_image_links as we are done with it.
+rm(til_image_links)
+
+features <- RPostgres::dbReadTable(.GlobalEnv$con, "features")
+groups <- RPostgres::dbReadTable(.GlobalEnv$con, "groups")
+
+samples_to_groups <- dplyr::tibble() %>% tibble::add_column(sample_id = NA, group_id = NA)
+features_to_samples <- dplyr::tibble() %>% tibble::add_column(feature_id = NA, sample_id = NA, value = NA)
+
+sample_set_01 <- all_samples %>%
+  dplyr::distinct(sample, TCGA_Study, TCGA_Subtype, Immune_Subtype, feature, value) %>%
+  dplyr::arrange(sample)
+
+# Remove the HUGE all_samples as we are done with it.
+rm(all_samples)
+
+for (row in 1:nrow(samples)) {
+  current_id = samples[row, "id"]
+  current_sample_id = samples[row, "sample_id"]
+  samples_to_groups <- samples_to_groups %>%
+    .GlobalEnv$rebuild_samples_to_groups(
+      current_id,
+      current_sample_id,
+      sample_set_01,
+      groups
+    )
+
+  features_to_samples <- features_to_samples %>%
+    .GlobalEnv$rebuild_features_to_samples(
+      current_id,
+      current_sample_id,
+      sample_set_01,
+      features
+    )
+}
+rm(row)
+rm(current_id)
+rm(current_sample_id)
+
+
+samples_to_groups %>% .GlobalEnv$write_table_ts(.GlobalEnv$con, "samples_to_groups", .)
+
+# Remove the data we are done with.
+rm(features_to_samples)
+rm(features)
+rm(groups)
+rm(samples)
+rm(sample_set_01)
+rm(samples_to_groups)
