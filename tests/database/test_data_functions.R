fd553e130a2c5691dca1b9f0abbf03f5bef6c315
Jon Ryser <241263+jonryser@users.noreply.github.com>
2019-12-20 15:36:25 -0800

    Missed file from last commit.

--- tests/database/test_data_functions.R
+++ tests/database/test_data_functions.R
@@ -20,35 +20,47 @@ library("testthat")
 # Make the custom data functions available.
 source("../../database/data_functions.R", chdir = TRUE)
 
+# is_df_empty
+testthat::test_that("is_df_empty returns FALSE when a valid non-empty dataframe or tibble is passed.", {
+  testthat::expect_that(is_df_empty(cars), is_identical_to(FALSE))
+  testthat::expect_that(is_df_empty(cars %>% dplyr::as_tibble()), is_identical_to(FALSE))
+})
+testthat::test_that("is_df_empty returns TRUE when an empty dataframe, empty tibble, NA, NULL, or no data frame is passed.", {
+  testthat::expect_that(is_df_empty(data.frame()), is_identical_to(TRUE))
+  testthat::expect_that(is_df_empty(NA), is_identical_to(TRUE))
+  testthat::expect_that(is_df_empty(NULL), is_identical_to(TRUE))
+  testthat::expect_that(is_df_empty(), is_identical_to(TRUE))
+})
+
+# link_to_references
+testthat::test_that("link_to_references returns NA when no value present.", {
+  testthat::expect_that(link_to_references(NA), is_identical_to(NA))
+})
+testthat::test_that("link_to_references returns NA when URL value is 'NA'.", {
+  link <- '<a href="NA">NA</a>'
+  testthat::expect_that(link_to_references(link), is_identical_to(NA))
+})
+testthat::test_that("link_to_references returns NA when there is no URL.", {
+  link <- '<a href>NA</a>'
+  testthat::expect_that(link_to_references(link), is_identical_to(NA))
+})
+testthat::test_that("link_to_references returns a url in curly braces.", {
+  url <- 'http://someplace.com?query=yes#pow'
+  link <- paste0('<a href="', url, '"></a>', sep = "")
+  expected <- paste0('{', url, '}', sep = "")
+  testthat::expect_that(link_to_references(link), is_identical_to(expected))
+})
+
 # switch_value
-testthat::test_that("switch_value return NA when no value present", {
+testthat::test_that("switch_value returns NA when no value present.", {
   reference <- dplyr::tibble(gene = c("1", "2", "3"), scoobs = c(NA, NA, NA))
   object <- dplyr::tibble(gene = c("", "", ""), scoobs = c("4", "5", "6"))
 
   testthat::expect_that(switch_value(reference[1,], "gene", "scoobs", object), is_identical_to(NA))
 })
-testthat::test_that("switch_value return the value from the second object.", {
+testthat::test_that("switch_value returns the value from the second object.", {
   reference <- dplyr::tibble(gene = c("1", "2", "3"), scoobs = c(NA, NA, NA))
   object <- dplyr::tibble(gene = c("1", "", ""), scoobs = c("4", "5", "6"))
 
   testthat::expect_that(switch_value(reference[1,], "gene", "scoobs", object), is_identical_to("4"))
-})
-
-# link_to_references
-testthat::test_that("link_to_references return NA when no value present", {
-  testthat::expect_that(link_to_references(NA), is_identical_to(NA))
-})
-testthat::test_that("link_to_references return NA when URL value is 'NA'", {
-  link <- '<a href="NA">NA</a>'
-  testthat::expect_that(link_to_references(link), is_identical_to(NA))
-})
-testthat::test_that("link_to_references return NA when there is no URL", {
-  link <- '<a href>NA</a>'
-  testthat::expect_that(link_to_references(link), is_identical_to(NA))
-})
-testthat::test_that("link_to_references return a url in curly braces", {
-  url <- 'http://someplace.com?query=yes#pow'
-  link <- paste0('<a href="', url, '"></a>', sep = "")
-  expected <- paste0('{', url, '}', sep = "")
-  testthat::expect_that(link_to_references(link), is_identical_to(expected))
 })
