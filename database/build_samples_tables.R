11d8dde8dc49e903a6556d634932dedf93fd1b5f
Jon Ryser <241263+jonryser@users.noreply.github.com>
2019-12-20 15:34:33 -0800

    Eliminating loops (as they are slow) and using dplyr joins instead.

--- database/build_samples_tables.R
+++ database/build_samples_tables.R
@@ -50,38 +50,35 @@ cat("Cleaned up.", fill = TRUE)
 gc()
 
 cat(crayon::magenta("Building samples_to_tags data."), fill = TRUE)
-samples_to_tags <- dplyr::tibble() %>% tibble::add_column(sample_id = NA %>% as.integer, tag_id = NA %>% as.integer)
 tags <- .GlobalEnv$read_table("tags") %>%
   dplyr::as_tibble() %>%
   dplyr::select(id, name)
-tags_length <- length(tags$name)
-tags <- tags %>% as.list
-samples_to_tags_sample_set <- all_samples %>% dplyr::distinct(sample, TCGA_Study, TCGA_Subtype, Immune_Subtype)
+sample_set_tcga_study <- all_samples %>%
+  dplyr::distinct(sample, TCGA_Study) %>%
+  dplyr::inner_join(tags, by = c("TCGA_Study" = "name")) %>%
+  dplyr::rename_at("id", ~("tag_id")) %>%
+  dplyr::distinct(sample, tag_id)
+sample_set_tcga_subtype <- all_samples %>%
+  dplyr::distinct(sample, TCGA_Subtype) %>%
+  dplyr::inner_join(tags, by = c("TCGA_Subtype" = "name")) %>%
+  dplyr::rename_at("id", ~("tag_id")) %>%
+  dplyr::distinct(sample, tag_id)
+sample_set_immune_subtype <- all_samples %>%
+  dplyr::distinct(sample, Immune_Subtype) %>%
+  dplyr::inner_join(tags, by = c("Immune_Subtype" = "name")) %>%
+  dplyr::rename_at("id", ~("tag_id")) %>%
+  dplyr::distinct(sample, tag_id)
+samples_to_tags <- sample_set_tcga_study %>%
+  dplyr::bind_rows(sample_set_tcga_subtype, sample_set_immune_subtype) %>%
+  dplyr::inner_join(samples, by = c("sample" = "sample_id")) %>%
+  dplyr::distinct(id, tag_id) %>%
+  dplyr::rename_at("id", ~("sample_id"))
+cat(crayon::blue("Built samples_to_tags data."), fill = TRUE)
 
-tags %>% purrr::pmap(~{
-  current_id <- ..1
-  current_tag_name <- ..2
-  row_number <- which(current_tag_name == tags$name)
-
-  svMisc::progress(row_number, tags_length - 1, progress.bar = TRUE)
-
-  sample_set <- samples_to_tags_sample_set %>%
-    dplyr::filter(TCGA_Study == current_tag_name | TCGA_Subtype == current_tag_name | Immune_Subtype == current_tag_name)
-
-  samples_to_tags <<- samples_to_tags %>%
-    .GlobalEnv$rebuild_samples_to_tags(current_id, current_tag_name, sample_set, samples)
-
-  if (row_number == tags_length) {
-    cat(crayon::blue("Built samples_to_tags data."), fill = TRUE)
-  }
-
-  rm(current_id)
-  rm(current_tag_name)
-  rm(row_number)
-  rm(sample_set)
-})
-
-rm(samples_to_tags_sample_set)
+rm(sample_set_tcga_study)
+rm(sample_set_tcga_subtype)
+rm(sample_set_immune_subtype)
+rm(tags)
 cat("Cleaned up.", fill = TRUE)
 gc()
 
@@ -90,46 +87,24 @@ table_written <- samples_to_tags %>% .GlobalEnv$write_table_ts("samples_to_tags"
 cat(crayon::blue("Built samples_to_tags table."), fill = TRUE)
 
 cat(crayon::magenta("Building samples_to_features data."), fill = TRUE)
-features_to_samples <- dplyr::tibble() %>%
-  tibble::add_column(
-    feature_id = NA %>% as.integer,
-    sample_id = NA %>% as.integer,
-    value = NA %>% as.numeric,
-    inf_value = NA %>% as.numeric
-  )
 features <- .GlobalEnv$read_table("features") %>%
   dplyr::as_tibble() %>%
   dplyr::select(id, name)
-features_length <- length(features$name)
-features <- features %>% as.list
-features_to_samples_sample_set <- all_samples %>% dplyr::select(sample, feature, value)
+sample_set_features <- all_samples %>%
+  dplyr::distinct(sample, feature, value) %>%
+  dplyr::inner_join(features, by = c("feature" = "name")) %>%
+  dplyr::rename_at("id", ~("feature_id")) %>%
+  dplyr::distinct(sample, feature_id, value)
+features_to_samples <- sample_set_features %>%
+  dplyr::inner_join(samples, by = c("sample" = "sample_id")) %>%
+  dplyr::distinct(id, feature_id, value) %>%
+  dplyr::rename_at("id", ~("sample_id")) %>%
+  dplyr::rowwise() %>%
+  dplyr::mutate(inf_value = ifelse(is.infinite(value), value, NA), value = ifelse(is.finite(value), value, NA))
+cat(crayon::blue("Built samples_to_features data."), fill = TRUE)
 
-features %>% purrr::pmap(~{
-  current_id <- ..1
-  current_feature_name <- ..2
-  row_number <- which(current_feature_name == features$name)
-
-  svMisc::progress(row_number, features_length - 1, progress.bar = TRUE)
-
-  sample_set <- features_to_samples_sample_set %>%
-    dplyr::select(sample, feature, value) %>%
-    dplyr::filter(feature == current_feature_name) %>%
-    dplyr::distinct(sample, feature, .keep_all = TRUE)
-
-  features_to_samples <<- features_to_samples %>%
-    .GlobalEnv$rebuild_features_to_samples(current_id, sample_set, samples)
-
-  if (row_number == features_length) {
-    cat(crayon::blue("Built samples_to_features data."), fill = TRUE)
-  }
-
-  rm(current_id)
-  rm(current_feature_name)
-  rm(row_number)
-  rm(sample_set)
-})
-
-rm(features_to_samples_sample_set)
+rm(sample_set_features)
+rm(features)
 cat("Cleaned up.", fill = TRUE)
 gc()
 
@@ -138,46 +113,21 @@ table_written <- features_to_samples %>% .GlobalEnv$write_table_ts("features_to_
 cat(crayon::blue("Built features_to_samples table."), fill = TRUE)
 
 cat(crayon::magenta("Building genes_to_samples data.\n(These are two large datasets, please be patient as they are rebuilt.)"), fill = TRUE)
-genes_to_samples <- dplyr::tibble() %>%
-  tibble::add_column(
-    gene_id = NA %>% as.integer,
-    sample_id = NA %>% as.integer,
-    status = NA %>% as.character,
-    rna_seq_expr = NA %>% as.numeric
-  )
 genes <- .GlobalEnv$read_table("genes") %>%
   dplyr::as_tibble() %>%
   dplyr::select(id, hgnc)
-genes_length <- length(genes$hgnc)
-genes <- genes %>% as.list
-genes_to_samples_sample_set <- all_samples %>% dplyr::as_tibble() %>% dplyr::select(sample, gene, status, rna_seq_expr)
-
-genes %>% purrr::pmap(~{
-  current_id <- ..1
-  current_hgnc <- ..2
-  row_number <- which(current_id == genes$id)
-
-  svMisc::progress(row_number, genes_length - 1, progress.bar = TRUE)
-
-  sample_set <- genes_to_samples_sample_set %>%
-    dplyr::filter(gene == current_hgnc) %>%
-    dplyr::distinct(sample, status, rna_seq_expr)
-
-  genes_to_samples <<- genes_to_samples %>% .GlobalEnv$rebuild_genes_to_samples(current_id, sample_set, samples)
-
-  if (row_number == genes_length) {
-    cat(crayon::blue("Built genes_to_samples data."), fill = TRUE)
-  }
-
-  rm(current_id)
-  rm(current_hgnc)
-  rm(row_number)
-  rm(sample_set)
-})
-
-rm(genes_to_samples_sample_set)
-cat("Cleaned up.", fill = TRUE)
-gc()
+sample_set_genes <- all_samples %>%
+  dplyr::distinct(sample, gene, status, rna_seq_expr) %>%
+  dplyr::inner_join(genes, by = c("gene" = "hgnc")) %>%
+  dplyr::rename_at("id", ~("gene_id")) %>%
+  dplyr::distinct(sample, gene_id, status, rna_seq_expr)
+genes_to_samples <- sample_set_genes %>%
+  dplyr::inner_join(samples, by = c("sample" = "sample_id")) %>%
+  dplyr::distinct(id, gene_id, status, rna_seq_expr) %>%
+  dplyr::group_by(id, gene_id) %>%
+  dplyr::summarise_all(funs(unique(.[.!='']))) %>%
+  dplyr::rename_at("id", ~("sample_id"))
+cat(crayon::blue("Built genes_to_samples data."), fill = TRUE)
 
 cat(crayon::magenta("Building genes_to_samples table.\n(There are many rows to write, this may take a little while.)"), fill = TRUE)
 table_written <- genes_to_samples %>% .GlobalEnv$write_table_ts("genes_to_samples")
@@ -185,13 +135,9 @@ cat(crayon::blue("Built genes_to_samples table."), fill = TRUE)
 
 # Remove the data we are done with.
 rm(features_to_samples)
-rm(features)
-rm(features_length)
 rm(genes)
-rm(genes_length)
 rm(genes_to_samples)
-rm(tags)
-rm(tags_length)
+rm(sample_set_genes)
 rm(samples_to_tags)
 rm(table_written)
 rm(all_samples)
