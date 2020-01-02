ddbd87a8789cec9a43c70efc7bbe24c4c49d1b94
Jon Ryser <241263+jonryser@users.noreply.github.com>
2020-01-02 15:06:06 -0800

    Building nodes tables.

--- database/build_nodes_tables.R
+++ database/build_nodes_tables.R
@@ -11,45 +11,53 @@ table_written <- nodes %>%
   .GlobalEnv$write_table_ts("node_names")
 cat(crayon::blue("Built the node_names table."), fill = TRUE)
 
-cat(crayon::magenta("Building the nodes table."), fill = TRUE)
+cat(crayon::magenta("Building the nodes data"), fill = TRUE)
 node_names <- .GlobalEnv$read_table("node_names") %>% dplyr::as_tibble()
-table_written <- nodes %>%
-  dplyr::select(Node, UpBinRatio) %>%
-  dplyr::rename_at("UpBinRatio", ~("ecn_value")) %>%
-  dplyr::inner_join(node_names, by = c("Node" = "name")) %>%
+nodes <- nodes %>%
+  dplyr::rename_at("Node", ~("name")) %>%
+  dplyr::inner_join(node_names, by = "name") %>%
   dplyr::rename_at("id", ~("node_name_id")) %>%
-  dplyr::select(-c(Node)) %>%
+  dplyr::rename_at("UpBinRatio", ~("ecn_value"))
+nodes$id <- seq.int(nrow(nodes))
+cat(crayon::blue("Built the nodes data"), fill = TRUE)
+
+cat(crayon::magenta("Building the nodes table."), fill = TRUE)
+table_written <- nodes %>%
+  dplyr::select(id, ecn_value, node_name_id) %>%
   .GlobalEnv$write_table_ts("nodes")
 cat(crayon::blue("Built the nodes table."), fill = TRUE)
 
 cat(crayon::magenta("Building the nodes_to_tags data"), fill = TRUE)
 tags <- .GlobalEnv$read_table("tags") %>% dplyr::as_tibble()
 
-current_pool <- .GlobalEnv$pool %>% pool::poolCheckout()
-nodes_db <- current_pool %>% 
-  dplyr::tbl("nodes") %>%
-  dplyr::inner_join(current_pool %>% dplyr::tbl("node_names"), by = c("node_name_id" = "id")) %>%
-  dplyr::as_tibble()
-current_pool %>% pool::poolReturn()
-
 node_set_group <- nodes %>%
+  dplyr::rename_at("id", ~("node_id")) %>%
   dplyr::inner_join(tags, by = c("Group" = "name")) %>%
   dplyr::rename_at("id", ~("tag_id")) %>%
-  dplyr::inner_join(nodes_db, by = c("Node" = "name")) %>%
-  dplyr::rename_at("id", ~("node_id")) %>%
   dplyr::distinct(node_id, tag_id)
 
 node_set_immune <- nodes %>%
+  dplyr::rename_at("id", ~("node_id")) %>%
   dplyr::inner_join(tags, by = c("Immune" = "name")) %>%
   dplyr::rename_at("id", ~("tag_id")) %>%
-  dplyr::inner_join(nodes_db, by = c("Node" = "name")) %>%
-  dplyr::rename_at("id", ~("node_id")) %>%
+  dplyr::distinct(node_id, tag_id)
+
+nodes_to_tags <- node_set_group %>%
+  dplyr::bind_rows(node_set_immune) %>%
   dplyr::distinct(node_id, tag_id)
 cat(crayon::blue("Built the nodes_to_tags data"), fill = TRUE)
 
-cat(crayon::magenta("Building the nodes_to_tags table."), fill = TRUE)
-table_written <- node_set_group %>%
-  dplyr::bind_rows(node_set_immune) %>%
-  dplyr::distinct(node_id, tag_id) %>%
-  .GlobalEnv$write_table_ts("nodes_to_tags")
+cat(crayon::magenta("Building the nodes_to_tags table.\n(There are", nrow(nodes_to_tags), "rows to write, this may take a little while.)"), fill = TRUE, sep = " ")
+table_written <- nodes_to_tags %>% .GlobalEnv$write_table_ts("nodes_to_tags")
 cat(crayon::blue("Built the nodes_to_tags table."), fill = TRUE)
+
+# Clean up.
+rm(nodes)
+rm(node_names)
+rm(tags)
+rm(node_set_group)
+rm(node_set_immune)
+rm(nodes_to_tags)
+rm(table_written)
+cat("Cleaned up.", fill = TRUE)
+gc()
