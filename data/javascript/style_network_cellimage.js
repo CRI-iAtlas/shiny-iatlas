480e73363c0537bd2ec180edaaa437838413fc65
heimannch <carolina.heimann@gmail.com>
2020-02-03 13:25:05 -0800

    added network and nodes abundance data

---
+++ data/javascript/style_network_cellimage.js
@@ -0,0 +1,423 @@
+[ {
+  "format_version" : "1.0",
+  "generated_by" : "cytoscape-3.7.2",
+  "target_cytoscapejs_version" : "~2.1",
+  "title" : "default",
+  "style" : [ {
+    "selector" : "node",
+    "css" : {
+      "width" : 75.0,
+      "text-opacity" : 1.0,
+      "text-valign" : "center",
+      "text-halign" : "center",
+      "color" : "rgb(0,0,0)",
+      "font-family" : "SansSerif",
+      "font-weight" : "normal",
+      "border-opacity" : 1.0,
+      "border-color" : "rgb(204,204,204)",
+      "background-color" : "mapData(UpBinRatio,0.125,0.25,rgb(229,245,224),rgb(199,233,192))",
+      "border-width" : 0.0,
+      "font-size" : 12,
+      "background-opacity" : 1.0,
+      "shape" : "roundrectangle",
+      "height" : 35.0,
+      "content" : "data(id)"
+    }
+  }, {
+    "selector" : "node[UpBinRatio = 1]",
+    "css" : {
+      "background-color" : "rgb(0,68,27)"
+    }
+  }, {
+    "selector" : "node[UpBinRatio > 0.875][UpBinRatio < 1]",
+    "css" : {
+      "background-color" : "mapData(UpBinRatio,0.875,1,rgb(0,109,44),rgb(0,68,27))"
+    }
+  }, {
+    "selector" : "node[UpBinRatio = 0.875]",
+    "css" : {
+      "background-color" : "rgb(0,109,44)"
+    }
+  }, {
+    "selector" : "node[UpBinRatio > 0.75][UpBinRatio < 0.875]",
+    "css" : {
+      "background-color" : "mapData(UpBinRatio,0.75,0.875,rgb(35,139,69),rgb(0,109,44))"
+    }
+  }, {
+    "selector" : "node[UpBinRatio = 0.75]",
+    "css" : {
+      "background-color" : "rgb(35,139,69)"
+    }
+  }, {
+    "selector" : "node[UpBinRatio > 0.625][UpBinRatio < 0.75]",
+    "css" : {
+      "background-color" : "mapData(UpBinRatio,0.625,0.75,rgb(65,171,93),rgb(35,139,69))"
+    }
+  }, {
+    "selector" : "node[UpBinRatio = 0.625]",
+    "css" : {
+      "background-color" : "rgb(65,171,93)"
+    }
+  }, {
+    "selector" : "node[UpBinRatio > 0.5][UpBinRatio < 0.625]",
+    "css" : {
+      "background-color" : "mapData(UpBinRatio,0.5,0.625,rgb(116,196,118),rgb(65,171,93))"
+    }
+  }, {
+    "selector" : "node[UpBinRatio = 0.5]",
+    "css" : {
+      "background-color" : "rgb(116,196,118)"
+    }
+  }, {
+    "selector" : "node[UpBinRatio > 0.375][UpBinRatio < 0.5]",
+    "css" : {
+      "background-color" : "mapData(UpBinRatio,0.375,0.5,rgb(161,217,155),rgb(116,196,118))"
+    }
+  }, {
+    "selector" : "node[UpBinRatio = 0.375]",
+    "css" : {
+      "background-color" : "rgb(161,217,155)"
+    }
+  }, {
+    "selector" : "node[UpBinRatio > 0.25][UpBinRatio < 0.375]",
+    "css" : {
+      "background-color" : "mapData(UpBinRatio,0.25,0.375,rgb(199,233,192),rgb(161,217,155))"
+    }
+  }, {
+    "selector" : "node[UpBinRatio = 0.25]",
+    "css" : {
+      "background-color" : "rgb(199,233,192)"
+    }
+  }, {
+    "selector" : "node[UpBinRatio > 0.125][UpBinRatio < 0.25]",
+    "css" : {
+      "background-color" : "mapData(UpBinRatio,0.125,0.25,rgb(229,245,224),rgb(199,233,192)"
+    }
+  }, {
+    "selector" : "node[UpBinRatio = 0.125]",
+    "css" : {
+      "background-color" : "rgb(229,245,224)"
+    }
+  }, {
+    "selector" : "node[UpBinRatio > 0][UpBinRatio < 0.125]",
+    "css" : {
+      "background-color" : "mapData(UpBinRatio,0,0.125,rgb(247,252,245),rgb(229,245,224))"
+    }
+  }, {
+    "selector" : "node[UpBinRatio = 0]",
+    "css" : {
+      "background-color" : "rgb(247,252,245)"
+    }
+  }, {
+    "selector" : "node[id = 'T_cells_CD8'][UpBinRatio > 0.875][UpBinRatio < 1]",
+    "css" : {
+      "background-color" : "mapData(UpBinRatio,0.875,1,rgb(153,52,4),rgb(102,37,6))"
+    }
+  }, {
+    "selector" : "node[id = 'T_cells_CD8'][UpBinRatio = 0.875]",
+    "css" : {
+      "background-color" : "rgb(153,52,4)"
+    }
+  }, {
+    "selector" : "node[id = 'T_cells_CD8'][UpBinRatio > 0.75][UpBinRatio < 0.875]",
+    "css" : {
+      "background-color" : "mapData(UpBinRatio,0.75,0.875,rgb(204,76,2),rgb(153,52,4)"
+    }
+  }, {
+    "selector" : "node[id = 'T_cells_CD8'][UpBinRatio = 0.75]",
+    "css" : {
+      "background-color" : "rgb(204,76,2)"
+    }
+  }, {
+    "selector" : "node[id = 'T_cells_CD8'][UpBinRatio > 0.625][UpBinRatio < 0.75]",
+    "css" : {
+      "background-color" : "mapData(UpBinRatio,0.625,0.75,rgb(236,112,20),rgb(204,76,2))"
+    }
+  }, {
+    "selector" : "node[id = 'T_cells_CD8'][UpBinRatio = 0.625]",
+    "css" : {
+      "background-color" : "rgb(236,112,20)"
+    }
+  }, {
+    "selector" : "node[id = 'T_cells_CD8'][UpBinRatio > 0.5][UpBinRatio < 0.625]",
+    "css" : {
+      "background-color" : "mapData(UpBinRatio,0.5,0.625,rgb(254,153,41),rgb(236,112,20))"
+    }
+  }, {
+    "selector" : "node[id = 'T_cells_CD8'][UpBinRatio = 0.5]",
+    "css" : {
+      "background-color" : "rgb(254,153,41)"
+    }
+  }, {
+    "selector" : "node[id = 'T_cells_CD8'][UpBinRatio > 0.375][UpBinRatio < 0.5]",
+    "css" : {
+      "background-color" : "mapData(UpBinRatio,0.375,0.5,rgb(254,196,79),rgb(254,153,41))"
+    }
+  }, {
+    "selector" : "node[id = 'T_cells_CD8'][UpBinRatio = 0.375]",
+    "css" : {
+      "background-color" : "rgb(254,196,79)"
+    }
+  }, {
+    "selector" : "node[id = 'T_cells_CD8'][UpBinRatio > 0.25][UpBinRatio < 0.375]",
+    "css" : {
+      "background-color" : "mapData(UpBinRatio,0.25,0.375,rgb(254,227,145),rgb(254,196,79))"
+    }
+  }, {
+    "selector" : "node[id = 'T_cells_CD8'][UpBinRatio = 0.25]",
+    "css" : {
+      "background-color" : "rgb(254,227,145)"
+    }
+  }, {
+    "selector" : "node[id = 'T_cells_CD8'][UpBinRatio > 0.125][UpBinRatio < 0.25]",
+    "css" : {
+      "background-color" : "mapData(UpBinRatio,0.125,0.25,rgb(255,247,188),rgb(254,227,145))"
+    }
+  }, {
+    "selector" : "node[id = 'T_cells_CD8'][UpBinRatio = 0.125]",
+    "css" : {
+      "background-color" : "rgb(255,247,188)"
+    }
+  }, {
+    "selector" : "node[id = 'T_cells_CD8'][UpBinRatio > 0][UpBinRatio < 0.125]",
+    "css" : {
+      "background-color" : "mapData(UpBinRatio,0,0.125,rgb(255,255,229),rgb(255,247,188))"
+    }
+  }, {
+    "selector" : "node[id = 'T_cells_CD8'][UpBinRatio = 0]",
+    "css" : {
+      "background-color" : "rgb(255,255,229)"
+    }
+  }, {
+    "selector" : "node[id = 'Tumor_cell'][UpBinRatio > 0.875][UpBinRatio < 1]",
+    "css" : {
+      "background-color" : "mapData(UpBinRatio,0.875,1,rgb(8,81,156),rgb(8,48,107))"
+    }
+  }, {
+    "selector" : "node[id = 'Tumor_cell'][UpBinRatio = 0.875]",
+    "css" : {
+      "background-color" : "rgb(8,81,156)"
+    }
+  }, {
+    "selector" : "node[id = 'Tumor_cell'][UpBinRatio > 0.75][UpBinRatio < 0.875]",
+    "css" : {
+      "background-color" : "mapData(UpBinRatio,0.75,0.875,rgb(33,113,181),rgb(8,81,156))"
+    }
+  }, {
+    "selector" : "node[id = 'Tumor_cell'][UpBinRatio = 0.75]",
+    "css" : {
+      "background-color" : "rgb(33,113,181)"
+    }
+  }, {
+    "selector" : "node[id = 'Tumor_cell'][UpBinRatio > 0.625][UpBinRatio < 0.75]",
+    "css" : {
+      "background-color" : "mapData(UpBinRatio,0.625,0.75,rgb(66,146,198),rgb(33,113,181))"
+    }
+  }, {
+    "selector" : "node[id = 'Tumor_cell'][UpBinRatio = 0.625]",
+    "css" : {
+      "background-color" : "rgb(66,146,198)"
+    }
+  }, {
+    "selector" : "node[id = 'Tumor_cell'][UpBinRatio > 0.5][UpBinRatio < 0.625]",
+    "css" : {
+      "background-color" : "mapData(UpBinRatio,0.5,0.625,rgb(107,174,214),rgb(66,146,198))"
+    }
+  }, {
+    "selector" : "node[id = 'Tumor_cell'][UpBinRatio = 0.5]",
+    "css" : {
+      "background-color" : "rgb(107,174,214)"
+    }
+  }, {
+    "selector" : "node[id = 'Tumor_cell'][UpBinRatio > 0.375][UpBinRatio < 0.5]",
+    "css" : {
+      "background-color" : "mapData(UpBinRatio,0.375,0.5,rgb(158,202,225),rgb(107,174,214))"
+    }
+  }, {
+    "selector" : "node[id = 'Tumor_cell'][UpBinRatio = 0.375]",
+    "css" : {
+      "background-color" : "rgb(158,202,225)"
+    }
+  }, {
+    "selector" : "node[id = 'Tumor_cell'][UpBinRatio > 0.25][UpBinRatio < 0.375]",
+    "css" : {
+      "background-color" : "mapData(UpBinRatio,0.25,0.375,rgb(198,219,239),rgb(158,202,225))"
+    }
+  }, {
+    "selector" : "node[id = 'Tumor_cell'][UpBinRatio = 0.25]",
+    "css" : {
+      "background-color" : "rgb(198,219,239)"
+    }
+  }, {
+    "selector" : "node[id = 'Tumor_cell'][UpBinRatio > 0.125][UpBinRatio < 0.25]",
+    "css" : {
+      "background-color" : "mapData(UpBinRatio,0.125,0.25,rgb(222,235,247),rgb(198,219,239))"
+    }
+  }, {
+    "selector" : "node[id = 'Tumor_cell'][UpBinRatio = 0.125]",
+    "css" : {
+      "background-color" : "rgb(222,235,247)"
+    }
+  }, {
+    "selector" : "node[id = 'Tumor_cell'][UpBinRatio > 0][UpBinRatio < 0.125]",
+    "css" : {
+      "background-color" : "mapData(UpBinRatio,0,0.125,rgb(247,251,255),rgb(222,235,247))"
+    }
+  }, {
+    "selector" : "node[id = 'Tumor_cell'][UpBinRatio = 0]",
+    "css" : {
+      "background-color" : "rgb(247,251,255)"
+    }
+  }, {
+    "selector" : "node[id = 'Dendritic_cells'][UpBinRatio > 0.875][UpBinRatio < 1]",
+    "css" : {
+      "background-color" : "mapData(UpBinRatio,0.875,1,rgb(84,39,143),rgb(63,0,125))"
+    }
+  }, {
+    "selector" : "node[id = 'Dendritic_cells'][UpBinRatio = 0.875]",
+    "css" : {
+      "background-color" : "rgb(84,39,143)"
+    }
+  }, {
+    "selector" : "node[id = 'Dendritic_cells'][UpBinRatio > 0.75][UpBinRatio < 0.875]",
+    "css" : {
+      "background-color" : "mapData(UpBinRatio,0.75,0.875,rgb(106,81,163),rgb(84,39,143))"
+    }
+  }, {
+    "selector" : "node[id = 'Dendritic_cells'][UpBinRatio = 0.75]",
+    "css" : {
+      "background-color" : "rgb(106,81,163)"
+    }
+  }, {
+    "selector" : "node[id = 'Dendritic_cells'][UpBinRatio > 0.625][UpBinRatio < 0.75]",
+    "css" : {
+      "background-color" : "mapData(UpBinRatio,0.625,0.75,rgb(128,125,186),rgb(106,81,163))"
+    }
+  }, {
+    "selector" : "node[id = 'Dendritic_cells'][UpBinRatio = 0.625]",
+    "css" : {
+      "background-color" : "rgb(128,125,186)"
+    }
+  }, {
+    "selector" : "node[id = 'Dendritic_cells'][UpBinRatio > 0.5][UpBinRatio < 0.625]",
+    "css" : {
+      "background-color" : "mapData(UpBinRatio,0.5,0.625,rgb(158,154,200),rgb(128,125,186))"
+    }
+  }, {
+    "selector" : "node[id = 'Dendritic_cells'][UpBinRatio = 0.5]",
+    "css" : {
+      "background-color" : "rgb(158,154,200)"
+    }
+  }, {
+    "selector" : "node[id = 'Dendritic_cells'][UpBinRatio > 0.375][UpBinRatio < 0.5]",
+    "css" : {
+      "background-color" : "mapData(UpBinRatio,0.375,0.5,rgb(188,189,220),rgb(158,154,200))"
+    }
+  }, {
+    "selector" : "node[id = 'Dendritic_cells'][UpBinRatio = 0.375]",
+    "css" : {
+      "background-color" : "rgb(188,189,220)"
+    }
+  }, {
+    "selector" : "node[id = 'Dendritic_cells'][UpBinRatio > 0.25][UpBinRatio < 0.375]",
+    "css" : {
+      "background-color" : "mapData(UpBinRatio,0.25,0.375,rgb(218,218,235),rgb(188,189,220))"
+    }
+  }, {
+    "selector" : "node[id = 'Dendritic_cells'][UpBinRatio = 0.25]",
+    "css" : {
+      "background-color" : "rgb(218,218,235)"
+    }
+  }, {
+    "selector" : "node[id = 'Dendritic_cells'][UpBinRatio > 0.125][UpBinRatio < 0.25]",
+    "css" : {
+      "background-color" : "mapData(UpBinRatio,0.125,0.25,rgb(239,237,245),rgb(218,218,235))"
+    }
+  }, {
+    "selector" : "node[id = 'Dendritic_cells'][UpBinRatio = 0.125]",
+    "css" : {
+      "background-color" : "rgb(239,237,245)"
+    }
+  }, {
+    "selector" : "node[id = 'Dendritic_cells'][UpBinRatio > 0][UpBinRatio < 0.125]",
+    "css" : {
+      "background-color" : "mapData(UpBinRatio,0,0.125,rgb(252,251,253),rgb(239,237,245))"
+    }
+  }, {
+    "selector" : "node[id = 'Dendritic_cells'][UpBinRatio = 0]",
+    "css" : {
+      "background-color" : "rgb(252,251,253)"
+    }
+  }, {
+    "selector" : "node[id = 'Macrophage'][UpBinRatio = 1]",
+    "css" : {
+      "background-color" : "rgb(0,69,41)"
+    }
+  }, {
+    "selector" : "node[id = 'Macrophage'][UpBinRatio > 0.875][UpBinRatio < 1]",
+    "css" : {
+      "background-color" : "mapData(UpBinRatio,0.875,1,rgb(0,104,55),rgb(0,69,41))"
+    }
+  }, {
+    "selector" : "node[id = 'Macrophage'][UpBinRatio > 0.75][UpBinRatio < 0.875]",
+    "css" : {
+      "background-color" : "mapData(UpBinRatio,0.75,0.875,rgb(35,132,67),rgb(0,104,55))"
+    }
+  }, {
+    "selector" : "node[id = 'Macrophage'][UpBinRatio > 0.625][UpBinRatio < 0.75]",
+    "css" : {
+      "background-color" : "mapData(UpBinRatio,0.625,0.75,rgb(65,171,93),rgb(35,132,67))"
+    }
+  }, {
+    "selector" : "node[id = 'Macrophage'][UpBinRatio > 0.5][UpBinRatio < 0.625]",
+    "css" : {
+      "background-color" : "mapData(UpBinRatio,0.5,0.625,rgb(120,198,121),rgb(65,171,93))"
+    }
+  }, {
+    "selector" : "node[id = 'Macrophage'][UpBinRatio > 0.375][UpBinRatio < 0.5]",
+    "css" : {
+      "background-color" : "mapData(UpBinRatio,0.375,0.5,rgb(173,221,142),rgb(120,198,121))"
+    }
+  }, {
+    "selector" : "node[id = 'Macrophage'][UpBinRatio > 0.25][UpBinRatio < 0.375]",
+    "css" : {
+      "background-color" : "mapData(UpBinRatio,0.25,0.375,rgb(217,240,163),rgb(173,221,142))"
+    }
+  }, {
+    "selector" : "node[id = 'Macrophage'][UpBinRatio > 0.125][UpBinRatio < 0.25]",
+    "css" : {
+      "background-color" : "mapData(UpBinRatio,0.125,0.25,rgb(247,252,185),rgb(217,240,163)"
+    }
+  }, {
+    "selector" : "node[id = 'Macrophage'][UpBinRatio >= 0][UpBinRatio < 0.125]",
+    "css" : {
+      "background-color" : "mapData(UpBinRatio,0,0.125,rgb(255,255,229),rgb(247,252,185))"
+    }
+  }, {
+    "selector" : "node:selected",
+    "css" : {
+      "background-color" : "rgb(255,255,0)"
+    }
+  }, {
+    "selector" : "edge",
+    "css" : {
+      "line-color": "black",
+        "target-arrow-shape": "triangle",
+        "target-arrow-color": "black",
+        "width": 0.5,
+        "curve-style": "bezier"
+    }
+  }, {
+    "selector" : "edge[interaction = 'NA']",
+    "css" : {
+      "line-color" : "rgb(0,0,0)",
+      "target-arrow-color" : "rgb(0,0,0)",
+      "source-arrow-color" : "rgb(0,0,0)"
+    }
+  }, {
+    "selector" : "edge:selected",
+    "css" : {
+      "line-color" : "rgb(255,0,0)"
+    }
+  } ]
+} ]
--- modules/cellimagemodule.R
+++ modules/cellimagemodule.R
@@ -1,3 +1,18 @@
+#Loading data for the network visualization
+cell_scaffold <- readr::read_tsv("data/network/scaffold_network_cellimage.tsv") %>% as.data.frame()
+
+net_data = list(
+  "immune"= feather::read_feather("data/network/node_cellimage_immune.feather"),
+  "subtype"= feather::read_feather("data/network/node_cellimage_subtype.feather"),
+  "study"= feather::read_feather("data/network/node_cellimage_study.feather")
+)
+      
+styleNodes = "data/javascript/style_network_cellimage.js"
+
+
+#Flag to change the cell image to ABUNDANCE data
+abundance <- TRUE
+  
 cellimage_UI <-function(id){
   
   ns <- NS(id)
@@ -22,13 +37,20 @@ cellimage_UI <-function(id){
             width = 6,
             uiOutput(ns("select_ui"))
           )
-        ),
-                
-        plotBox(
-          width = 8,
-          plotOutput(ns("cellPlot"), height = 600) %>%
-          shinycssloaders::withSpinner()
         )
+        ),
+        fluidRow(
+          plotBox(
+            width = 5,
+            plotOutput(ns("cellPlot"), height = 600) %>%
+              shinycssloaders::withSpinner()
+          ),
+          plotBox(
+            width = 7,
+            cyjShiny::cyjShinyOutput(ns("image_network"), height = 600)%>% 
+              shinycssloaders::withSpinner()
+          )
+                
       )
     ) # closes sectionBox
     
@@ -76,8 +98,17 @@ cellimage <- function(
     )
     
   })
+  
+  #organizing data to network visualization
+  
+  nodes_ratio <- reactive({
+    if(group_internal_choice() == "Subtype_Immune_Model_Based") net_data$immune
+    else if(group_internal_choice() == "Subtype_Curated_Malta_Noushmehr_et_al") net_data$subtype
+    else net_data$study
+  })
 
   output$cellPlot <- renderPlot({
+    req(nodes_ratio())
     
     group_col <- group_internal_choice()
     group_df <- sample_group_df() %>% dplyr::mutate(Tumor_Fraction=1-Stromal_Fraction)
@@ -92,7 +123,6 @@ cellimage <- function(
     ### Value: the average value in that subtype and for that variable
     all_vals_df <- generate_value_df(group_df,group_col,cois,gois)
     ## all_vals_df could be replaced using another value for subtype summarization 
-    
     availability <- all_vals_df %>% dplyr::group_by(Group,Variable) %>% dplyr::summarise(Count=length(Value)) %>% 
       dplyr::group_by(Group) %>% dplyr::summarize(MinCount=min(Count))
     
@@ -107,13 +137,60 @@ cellimage <- function(
       if (scount <= 3 ){ enough_data=FALSE }
     }
     
-    if(enough_data){
-      image_grob <- get_colored_image(subtype_selected,cellimage_base,all_vals_df)
-      grid::grid.draw(image_grob)
-    } else {
-      print("Please select another subtype - this one has limited data") ## This needs to go to the app , not to the terminal
+    shiny::validate(need((enough_data == TRUE), "Please select another subtype - this one has limited data."))
+    
+    #The availability test is still based on the original all_vals_df, only after the test we change the data to abundance values
+    if (abundance == TRUE){
+      all_vals_df <- nodes_ratio()
     }
     
+    image_grob <- get_colored_image(subtype_selected,cellimage_base,all_vals_df)
+    grid::grid.draw(image_grob)
+    
+    # if(enough_data){
+    #   image_grob <- get_colored_image(subtype_selected,cellimage_base,all_vals_df)
+    #   grid::grid.draw(image_grob)
+    # } else {
+    #   print("Please select another subtype - this one has limited data") ## This needs to go to the app , not to the terminal
+    # }
+  })
+  
+##Network visualization
+  
+  tbl_edges <- reactive({ #doesn't need to be reactive, actually
+    
+    network <- cell_scaffold %>%
+      dplyr::mutate(interaction = "interacts with")
+    
+    colnames(network) <- c("source", "target", "interaction") #names required by cyjShiny package
+    
+    network
+  })
+  
+  ##Getting the nodes annotation to send to cyjShiny
+  tbl_nodes <- reactive({
+    req(nodes_ratio(), tbl_edges())
+    
+    nodes <- nodes_ratio()
+    
+    nodes$Variable <- gsub("T_cells_CD8.Aggregate2", "T_cells_CD8", nodes$Variable)
+    nodes$Variable <- gsub("Dendritic_cells.Aggregate1", "Dendritic_cells",  nodes$Variable)
+    nodes$Variable <- gsub("Macrophage.Aggregate1", "Macrophage", nodes$Variable)
+    
+    nodes %>%
+      filter(Group == input$groupselect_method) %>% 
+      rename(id = Variable, UpBinRatio = Value) %>% 
+      select(id, UpBinRatio) %>% 
+      arrange(id)
+    
+  })
+  
+  graph.json <- reactive({
+    cyjShiny::dataFramesToJSON(tbl_edges(), tbl_nodes())
+  })
+  
+  output$image_network <- cyjShiny::renderCyjShiny({
+    cyjShiny::cyjShiny(graph.json(), layoutName = "cose", styleFile = styleNodes)
   })
     
 }
